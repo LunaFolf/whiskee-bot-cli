7f1ee964c0fdc7bafacdba68c4cb0176019b5f50
Liam Holmes <email@liamholmes.co.uk>
2020-05-30 20:42:44 +0100

    added additional logging

--- cli.js
+++ cli.js
@@ -10,7 +10,7 @@ if (args.length && args[0].length >= 4) {
     var commandClassName = commandNameRaw.slice(0, 1).toUpperCase() + commandNameRaw.slice(1)
     if (!commandClassName.toLowerCase().endsWith('command')) commandClassName = (commandClassName + "Command")
     console.log('Creating a command class called ' + commandClassName)
-    console.log('Checking if command already exists')
+    console.log('Checking if ' + commandClassName + ' already exists')
     if (fs.existsSync('./commands/' + commandClassName)) {
         console.error('Command already exists, use a different name.')
     } else {
@@ -22,14 +22,20 @@ if (args.length && args[0].length >= 4) {
 
 function createTempFolder () {
     const tempLocation = path.join(__dirname, "/temp");
-    if (!fs.existsSync(tempLocation)) fs.mkdirSync(tempLocation)
+    if (!fs.existsSync(tempLocation)) {
+        console.warn('The temp location didn\'t exist so we\'re creating it at ' + tempLocation)
+        fs.mkdirSync(tempLocation);
+    }
 }
 
 function createNewClassFolder(className) {
     if (className) {
         createTempFolder()
         const classLocation = path.join(__dirname, "/temp/" + className)
-        if (!fs.existsSync(classLocation)) fs.mkdirSync(classLocation);
+        if (!fs.existsSync(classLocation)) {
+            console.log('Creating Temp directory for class')
+            fs.mkdirSync(classLocation);
+        }
     }
 }
 
@@ -39,6 +45,7 @@ async function createNewClassFile(className) {
         const defaultClassLocation = path.join(__dirname, "/DefaultClass.js");
         const tempLocation = path.join(__dirname, "temp", className, "/index.js");
 
+        console.log('Copying DefaultClass into temp class directory')
         fs.copyFile(defaultClassLocation, tempLocation, (err) => {
         if (err) throw err;
         renameClassFileInternals(className)
@@ -47,6 +54,7 @@ async function createNewClassFile(className) {
 }
 
 function renameClassFileInternals(className) {
+    console.log('Renaming internal class names')
     fs.readFile(
       path.join(__dirname, "temp", className, "/index.js"),
       "utf8",
@@ -74,6 +82,7 @@ function copyFileToSource(className) {
       "./commands/" + className + "/index.js",
       (err) => {
         if (err) throw err;
+        console.log("Copied new class to source directory");
       }
     );
 }
